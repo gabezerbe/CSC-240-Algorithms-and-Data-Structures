01ba592bad1447d390d513c7cc55fc31ed1e1822
Gabe Zerbe <gabe.zerbe@gmail.com>
2022-02-08 19:39:07 -0500

    added several sample sizes and nanoSecond walltimer

--- CSC-240/ApplicationProblem1/src/com/company/Main.java
+++ CSC-240/ApplicationProblem1/src/com/company/Main.java
@@ -3,15 +3,26 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int X[] = {100, 200, 300, 400, 500};
+//        int X[] = {100, 200, 300, 400, 500};
+//        int X[] = {100, 200, 300, 400, 500, 600, 700, 800, 900, 1000};
+//        int X[] = {100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500};
+//            int X[] = {100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800, 1900, 2000};
+            int X[] = {100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800, 1900, 2000, 2100, 2200, 2300, 2400, 2500};
         int n = X.length;
 
-        System.out.println("ap1 results:");
+//        System.out.println("ap1 results:");
+        long startTime = System.nanoTime();
         ap1(X, n);
+        long endTime = System.nanoTime();
+        long duration = (endTime - startTime);
+        System.out.println("ap1 runtime: " + duration);
+//        System.out.println("ap2 results:");
 
-        System.out.println("ap2 results:");
+        long startTime2 = System.nanoTime();
         ap2(X, n);
-
+        long endTime2 = System.nanoTime();
+        long duration2 = endTime2 - startTime2;
+        System.out.println("ap2 runtime: " + duration2);
     }
 
     static void ap1(int[] X, int n){
@@ -25,9 +36,9 @@ public class Main {
             A[i] = a / (i + 1);
         }
 
-        for (int element: A){
-            System.out.println(element);
-        }
+//        for (int element: A){
+//            System.out.println(element);
+//        }
     }
 
     static void ap2(int X[], int n){
@@ -39,8 +50,8 @@ public class Main {
             A[i] = s/(i+1);
         }
 
-        for (int element: A){
-            System.out.println(element);
-        }
+//        for (int element: A){
+//            System.out.println(element);
+//        }
     }
 }
