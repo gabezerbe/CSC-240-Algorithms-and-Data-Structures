f6d09e072709e6e9fde690be326aef13d31cee3a
Gabe Zerbe <gabe.zerbe@gmail.com>
2022-03-13 19:25:35 -0400

    removed passing of private variables to methods and adjusted output formatting to properly display 2 decimal places

--- ApplicationProblem2/src/Portfolio.java
+++ ApplicationProblem2/src/Portfolio.java
@@ -1,8 +1,6 @@
 import java.io.*;
 import java.net.URL;
-import java.sql.Array;
 import java.util.*;
-
 
 /**
  * Capital gains calculator
@@ -17,51 +15,61 @@ public class Portfolio {
     private ArrayList<String> stockNames = new ArrayList<String>();
     private ArrayList<Stock> stocks = new ArrayList<Stock>();
 
-    /**
-     * Our stock class that will store its symbol and transaction history
-     */
 
+    /**
+     * Stock class - Stock class that encapsulates our 'Stock' data including the symbol, and its transactions
+     */
     private class Stock{
         private String symbol;
         private Queue<Transaction> transactions = new LinkedList<>();
 
+        /**
+         *
+         * @param symbol - The '$STOCK' symbol that is stored for being checked later to make sure we don't have duplicate stocks being stored
+         *               in our storage array
+         */
         public Stock(String symbol) {
             this.symbol = symbol;
 
         }
 
+        /**
+         * Getter method for retrieving a stock's symbol
+         * @return
+         */
         public String getSym(){
             return this.symbol;
         }
 
-        public void Sell(Transaction t){
-            float purchaseAmount = 0f;
-            float sellAmount = 0f;
-            float gains = 0f;
+        /**
+         * Parses our 'SELL' transaction objects and performs some fun math to make sure the stocks are being sold in the correct order
+         * @param t - transaction object to be parsed
+         */
+        public void sell(Transaction t){
+            double purchaseAmount = 0d;
+            double sellAmount = 0d;
+            double gains = 0d;
 
-            System.out.println("SELL " + t.amount + " " + t.stock + " " + t.price);
+            System.out.println("SELL " + t.amount + " " + t.stock + String.format(" %.2f", t.price));
             while(t.amount != 0){
                 Transaction r = transactions.remove();
                 if(r.amount <= t.amount){
                     t.amount -= r.amount;
                     purchaseAmount += r.amount * r.price;
-                    System.out.println("\tSELL " + r.amount + " " + t.stock + " " + r.price);
+                    System.out.println("\tSELL " + r.amount + " " + t.stock + String.format(" %.2f", r.price));
                 } else {
                     r.amount -= t.amount;
                     purchaseAmount += t.amount * r.price;
-                    System.out.println("\tSELL " + t.amount + " " + t.stock + " " + r.price);
+                    System.out.println("\tSELL " + t.amount + " " + t.stock + String.format(" %.2f", r.price));
                     t.amount = 0;
                 }
                 sellAmount += r.amount * t.price;
 
             }
-            /**
-             * I have no idea why, but it's setting the decimal points both to zero despite being
-             * two floats that are getting multiplied
-             */
+
             gains = sellAmount - purchaseAmount;
 
-            if(gains >= 0f){
+            if(gains >= 0d){
                 System.out.printf("GAINS: %.2f %n", gains);
             } else {
                 System.out.printf("LOSSES: %.2f %n", gains);
@@ -69,24 +77,30 @@ public class Portfolio {
 
         }
 
-        public void Buy(Transaction t){
+        /**
+         * Parses our 'BUY' transaction objects
+         * @param t - transaction object to be parsed
+         */
+
+        public void buy(Transaction t){
             transactions.add(t);
-            System.out.println("BUY " + t.amount + " " + t.stock + " " + t.price);
+
+            System.out.println("BUY " + t.amount + " " + t.stock + String.format(" %.2f", t.price));
         }
 
     }
 
     /**
-     * Transaction class for storing our transactions
+     * Transaction class for storing our transaction strings as an object
      */
 
     private class Transaction{
         private String action;
         private int amount;
         private String stock;
-        private float price;
+        private double price;
 
-        public Transaction(String action, int amount, String stock, float price){
+        public Transaction(String action, int amount, String stock, double price){
             this.action = action;
             this.amount = amount;
             this.stock = stock;
@@ -95,11 +109,11 @@ public class Portfolio {
 
     }
 
-    /**;
-     * Reads in our transactions.txt
-     * @return
+    /**
+     *
+     * @param file - file name from user input
      */
-    public void readTransactions(String file, ArrayList<String> stockNames){
+    public void readTransactions(String file){
         BufferedReader reader;
         try{
             URL path = Portfolio.class.getResource(file);
@@ -116,17 +130,17 @@ public class Portfolio {
                 Transaction t = new Transaction(brokenLine[0], Integer.parseInt(brokenLine[1]),
                         brokenLine[2], Float.parseFloat(brokenLine[3]));
 
-                if(!stockNames.contains(brokenLine[2])){
-                    stocks.add(new Stock(brokenLine[2]));
-                    stockNames.add(brokenLine[2]);
+                if(!this.stockNames.contains(brokenLine[2])){
+                    this.stocks.add(new Stock(brokenLine[2]));
+                    this.stockNames.add(brokenLine[2]);
                 }
 
-                for (Stock s : stocks){
+                for (Stock s : this.stocks){
                     if(brokenLine[2].equals(s.getSym())){
                         if(brokenLine[0].equals("BUY")){
-                            s.Buy(t);
+                            s.buy(t);
                         } else if (brokenLine[0].equals("SELL")){
-                            s.Sell(t);
+                            s.sell(t);
                         } else {
                             System.out.println("ERROR: Please use either SELL or BUY for your action!");
                             System.exit(0);
@@ -156,7 +170,7 @@ public class Portfolio {
         Portfolio p = new Portfolio();
         PrintStream o = new PrintStream(new File("transaction_record.txt"));
         System.setOut(o);
-        p.readTransactions(str, p.stockNames);
+        p.readTransactions(str);
 
     }
 }
